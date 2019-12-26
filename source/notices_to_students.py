4654ef7b77244c94560cdde87331c65f96d53626
ARSH SHARMA <184517@nith.ac.in>
2019-12-27 02:53:01 +0530

    add notices-to-students

---
+++ source/notices_to_students.py
@@ -0,0 +1,24 @@
+import requests
+from dateutil.parser import parse
+from bs4 import BeautifulSoup
+
+def notices_to_students():
+    url = "http://nith.ac.in/?page_id=14888"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html5lib') 
+    arr = []
+    table = soup.find_all('table', {"align":"center"})#.tbody.find('tr') 
+    table1 = table[0].find_all('tbody')
+    table_fin = table1[0].find_all('tr')
+
+    arr =[]
+    for element in table_fin:
+            obj = {}
+            tds = (element.findAll('td'))#[0]#.findAll('div')
+            tds_in = tds[0].findAll('b')
+            obj['Title'] = tds_in[0].contents[0].text
+            obj['link'] = tds_in[0].contents[0]['href']
+            obj['Date of Upload'] = tds[3].contents[0]
+            print(obj)
+            arr.append(obj) 
+    return arr
