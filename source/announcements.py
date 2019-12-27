9b13ab2448fe3630b31de79053f8e4a4740b6fb3
Arsh Sharma <43717431+Sov-trotter@users.noreply.github.com>
2019-12-27 14:34:32 +0530

    Update announcements.py

--- source/announcements.py
+++ source/announcements.py
@@ -6,11 +6,11 @@ def announcements():
     url = "https://nith.ac.in"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html5lib') 
-    arr = []
+    announce = []
     table = soup.find_all('ul', {"style":"text-align:left; font-size: 20px"})#tbody.findAll('tr')   
     #table = soup.find_all('table', {"class":"dataTable"})[1].tbody.findAll('tr')
     i = 0
-    arr =[]
+    announce =[]
     for element in table:{}
     for i in range(0,7):
             obj = {}
@@ -18,5 +18,5 @@ def announcements():
             obj['Title'] = tds[i].contents[0].text
             obj['link'] = tds[i].contents[0]['href']
             i += 1
-            arr.append(obj) 
-    return arr
+            announce.append(obj) 
+    return announce
