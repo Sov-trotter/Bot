63b53ab93ba28ea3538624372f3890038fd4e3f1
ARSH SHARMA <184517@nith.ac.in>
2019-12-27 14:57:46 +0530

    add past events

--- source/past_events.py
+++ source/past_events.py
@@ -2,18 +2,18 @@ import requests
 from dateutil.parser import parse
 from bs4 import BeautifulSoup
 
-def upcom   ing_events():
+def past_events():
     url = "http://nith.ac.in/?page_id=23481"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html5lib') 
-    upcoming_event = []
-    table = soup.find_all('table', {"width":"100%"})[0].tbody.findAll('tr') #meta property="og:description"
+    past_event = []
+    table = soup.find_all('table', {"width":"100%"})[1].tbody.findAll('tr') #meta property="og:description"
     for element in table:
         obj = {}
         tds = (element.findAll('td'))
         obj['Title'] = tds[0].text
         obj['Date of Event'] = tds[1].text
         obj['Brochure link'] = "https://nith.ac.in" + tds[2].contents[0]['href']
-        upcoming_event.append(obj)
-    return(upcoming_event)
+        past_event.append(obj)
+    return(past_event)
     
