4bd811f18c9e62be47bd0861d36af83bdf9aa266
Arsh Sharma <43717431+Sov-trotter@users.noreply.github.com>
2019-12-27 14:35:36 +0530

    Update notices_to_students.py

--- source/notices_to_students.py
+++ source/notices_to_students.py
@@ -6,12 +6,12 @@ def notices_to_students():
     url = "http://nith.ac.in/?page_id=14888"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html5lib') 
-    arr = []
+    notice_students = []
     table = soup.find_all('table', {"align":"center"})#.tbody.find('tr') 
     table1 = table[0].find_all('tbody')
     table_fin = table1[0].find_all('tr')
 
-    arr =[]
+    notice_students =[]
     for element in table_fin:
             obj = {}
             tds = (element.findAll('td'))#[0]#.findAll('div')
@@ -20,5 +20,5 @@ def notices_to_students():
             obj['link'] = tds_in[0].contents[0]['href']
             obj['Date of Upload'] = tds[3].contents[0]
             print(obj)
-            arr.append(obj) 
-    return arr
+            notice_students.append(obj) 
+    return notice_students
